5f60e078080ea168179db28a96f47ecb74610e90
enjaku4 <enjaku4@gmail.com>
2023-07-03 09:41:00 +0200

    results server

--- lib/duper.ex
+++ lib/duper.ex
@@ -1,18 +1,2 @@
 defmodule Duper do
-  @moduledoc """
-  Documentation for `Duper`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Duper.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
