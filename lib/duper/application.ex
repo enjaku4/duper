62777f4b63dfb10d2b1551911f8ca20015092182
enjaku4 <enjaku4@gmail.com>
2023-07-12 22:27:49 +0200

    gatherer server

--- lib/duper/application.ex
+++ lib/duper/application.ex
@@ -10,12 +10,13 @@ defmodule Duper.Application do
     children = [
       Duper.Results,
       {Duper.PathFinder, "."},
-      Duper.WorkerSupervisor
+      Duper.WorkerSupervisor,
+      {Duper.Gatherer, 1}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    opts = [strategy: :one_for_all, name: Duper.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
